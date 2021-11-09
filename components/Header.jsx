1a06b067c38f67bcb2aa9e799516c7019114fc4e
allistermugaisi <allistermugaisi@gmail.com>
2021-11-09 07:54:39 +0300

    updated blog landing ui

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,13 +17,14 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="cursor-pointer font-bold text-4xl text-white">Our Blog</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
           ))}
+          <Link href="https://www.tomngoelaw.co.ke"><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">Home</span></Link>
         </div>
       </div>
     </div>
